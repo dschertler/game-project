51f33ea516a41b06b5e51de9e7126c8bc24c1951
Duncan <Duncan@DESKTOP-R3O8394>
2016-09-05 16:12:10 -0400

    Implemented a vertex handler managed through VAOs and VBOs

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -4,17 +4,39 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 		//Create the initial display on launch
 		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		//Keep updating display until user closes application
 		while(!Display.isCloseRequested()){
-			
+			renderer.prepare();
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
+		loader.cleanUp();
 		//Close the application
 		DisplayManager.closeDisplay();
 	}
